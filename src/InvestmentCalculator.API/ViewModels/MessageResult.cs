a5d7a5f53c7ba5b3606cacc283109a49d36f3058
Leandro Almeida <leandrualmeida@gmail.com>
2023-01-25 14:39:30 -0300

    -Adjustments on .sln

---
+++ src/InvestmentCalculator.API/ViewModels/MessageResult.cs
@@ -0,0 +1,14 @@
+﻿namespace InvestmentCalculator.API.ViewModels
+{
+    public class MessageResult
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+
+        public MessageResult(string code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+    }
+}
---
+++ tests/InvestmentCalculator.Tests/CalculatorControllerTests.cs
@@ -0,0 +1,11 @@
+namespace InvestmentCalculator.Tests
+{
+    public class CalculatorControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
