a02fbc3f0440751910b896598b2b560a1f7ea8a8
Leandro Almeida <leandrualmeida@gmail.com>
2023-02-06 16:14:24 -0300

    Testing globalization on tests

--- tests/InvestmentCalculator.Tests/Usings.cs
+++ tests/InvestmentCalculator.Tests/Usings.cs
@@ -1,1 +1,5 @@
 global using Xunit;
+using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("pt-BR");
